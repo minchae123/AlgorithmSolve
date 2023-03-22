01d54a9bfb10ab0112a24905e0513bee103b47af
minchae <98925762+minchae123@users.noreply.github.com>
2023-03-22 15:03:12 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	string s;
+	cin >> s;
+	for (char i = 'a'; i <= 'z'; i++) {
+		cout << (int)s.find(i) << ' ';
+	}
+}

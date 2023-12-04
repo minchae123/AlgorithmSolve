ed63f4f079d4e5bc1100fb712d547245734ba86b
minchae <98925762+minchae123@users.noreply.github.com>
2023-12-04 10:30:48 +0900

    [Silver V] Title: 파스칼의 삼각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/16395. 파스칼의 삼각형/파스칼의 삼각형.cc
+++ 백준/Silver/16395. 파스칼의 삼각형/파스칼의 삼각형.cc
@@ -11,7 +11,7 @@ int main()
     {
         for (int j = 1; j <= i; j++)
         {
-            if (j == 0 || j == i)
+            if (j == 1 || j == i)
                 {
                     v[i][j] = 1;
                 }

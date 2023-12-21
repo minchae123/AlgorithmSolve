566d415e4d9cd3498ca1a7de37e910262f4dd6ae
minchae123 <08minchae17@gmail.com>
2023-12-21 10:18:55 +0900

    4

--- Solve/Solve/가까운수.cpp
+++ Solve/Solve/가까운수.cpp
@@ -4,5 +4,18 @@ using namespace std;
 
 int main()
 {
-	int n, k, sum = 0;
+	int n, k, check = INT_MAX;
+	cin >> n >> k;
+	
+    int ans = 0;
+    for (int i = 0; i < n; i++)
+    {
+        int  a;
+        cin >> a;
+        if (abs(a - k) < check)
+        {
+            ans = a;
+        }
+	}
+	cout << ans;
 }
